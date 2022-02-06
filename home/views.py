dba892b12ad2fa2c68112457494c0dbd23ccf967
Jonathan Matarazzi <git@jonathanm.nl>
2022-02-06 15:35:20 +0100

    Made video full width and height and added scroll down button

--- home/views.py
+++ home/views.py
@@ -11,7 +11,7 @@ from events.models import Event, sync_events
 def index(request):
     sync_events()
     news = get_recent_articles()
-    events = Event.objects.all().filter(start__gte=datetime.today()).order_by('start')
+    events = Event.objects.all().filter(start__gte=datetime.today()).order_by('start')[:5]
 
     return render(request, 'home/index.html', {
         'news': news,
